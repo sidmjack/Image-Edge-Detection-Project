a47d89930263d9ea2520e79863bd2958658eb7cf
Sidney Jackson <sidneyjackson@sidneys-mbp.mtw.jhu.edu>
2016-04-17 18:17:52 -0400

    Added modified Edge class -> called WEdge

---
+++ WEdge.java
@@ -0,0 +1,122 @@
+/** Implementation of an Wedge class (for graphs), could be directed or not.
+ */
+public class WEdge<T> implements Comparable<WEdge<T>> {
+
+    /** Starting Gvertex<T> of an Wedge. */
+    private GVertex<T> source;
+    /** Ending Gvertex<T> of an Wedge. */
+    private GVertex<T> end;
+    /** Whether or not the Wedge is directed. */
+    private boolean directed;
+    /** Edge Weight */
+    private double weight;
+
+    /** Create an undirected Wedge.
+     *  @param u the start
+     *  @param v the end
+     */
+    public WEdge(GVertex<T> u, GVertex<T> v, double w) {
+        this.source = u;
+        this.end = v;
+        this.directed = false;
+        this.weight = w;
+    }
+
+    /** Create an Wedge.
+     *  @param u the start
+     *  @param v the end
+     *  @param dir true if directed, false otherwise
+     */
+    public WEdge(GVertex<T> u, GVertex<T> v, boolean dir, double w) {
+        this.source = u;
+        this.end = v;
+        this.directed = dir;
+        this.weight = w;
+    }
+
+    /** Is the Wedge directed.
+     *  @return true if yes, false otherwise
+     */
+    public boolean isDirected() {
+        return this.directed;
+    }
+
+    /** Is a Gvertex<T> incident to this Wedge.
+     *  @param v the Gvertex<T>
+     *  @return true if source or end, false otherwise
+     */
+    public boolean isIncident(GVertex<T> v) {
+        return this.source.equals(v) || this.end.equals(v);
+    }
+
+    /** Get the starting endpoint Gvertex<T>.
+     *  @return the Gvertex<T>
+     */
+    public GVertex<T> source() {
+        return this.source;
+    }
+
+    /** Get the ending endpoint Gvertex<T>.
+     *  @return the Gvertex<T>
+     */
+    public GVertex<T> end() {
+        return this.end;
+    }
+
+    /** Get the weight of Wedge.
+     *  @return the weight
+     */
+    public double weight() {
+        return this.weight;
+    }
+
+    /** Create a string representation of the Wedge.
+     *  @return the string as (source,end)
+     */
+    public String toString() {
+        return "(" + this.source + "," + this.end + "," + this.weight + ")";
+    }
+
+    /**
+     * Compares the weights of two different WEdges.
+     * @param  that WEdge.
+     * @return Returns -1 if this less than that, 1 f this is greater than that
+     *         and returns 0 if this is equal to that.
+     */
+    public int compareTo(WEdge<T> that) {
+       if (this.weight > that.weight) {
+            return 1;
+       } else if (this.weight < that.weight) {
+            return -1;
+       } else {
+            return 0;
+       }
+    }
+
+    /** Check if two Wedges are the same.
+     *  @param other the Wedge to compare to this
+     *  @return true if directedness and endpoints match, false otherwise
+     */
+    public boolean equals(WEdge<T> that) {
+        if (this.directed != that.directed) {
+            return false;
+        }
+        if (this.directed) {
+            return this.source.equals(that.source)
+                && this.end.equals(that.end);
+        } else {
+            return this.source.equals(that.source)
+                && this.end.equals(that.end)
+                || this.source.equals(that.end)
+                && this.end.equals(that.source);
+        }
+    }
+
+    /** Make a hashCode based on the toString.
+     *  @return the hashCode
+     */
+    public int hashCode() {
+        return this.toString().hashCode();
+    }
+
+}
