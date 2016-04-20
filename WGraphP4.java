3fb687a7f1a134f75e207abee46c3ef39b7596ae
Lawrence <lawrence@jhu.edu>
2016-04-20 18:11:37 -0400

    associated vertex function

--- WGraphP4.java
+++ WGraphP4.java
@@ -61,8 +61,7 @@ public class WGraphP4<VT> implements WGraph<VT> {
      */
     @Override
     public boolean addVertex(GVertex<VT> v) {
-        boolean alreadyThere = this.verts.contains(v);
-        if (alreadyThere) {
+        if (this.vertexInGraph(v)) {
             return false;
         } else {
             return this.verts.add(v);
@@ -141,12 +140,16 @@ public class WGraphP4<VT> implements WGraph<VT> {
      */
     @Override
     public List<GVertex<VT>> neighbors(GVertex<VT> v) {
-        Set<Integer> idSet = this.edges.get(v.id()).keySet();
-        List<GVertex<VT>> neighborList = new ArrayList<GVertex<VT>>(idSet.size());
-        for (Integer id : idSet) {
-            neighborList.add(this.verts.get(id));
+        if (this.vertexInGraph(v)) {
+            Set<Integer> idSet = this.edges.get(v.id()).keySet();
+            List<GVertex<VT>> neighborList = new ArrayList<GVertex<VT>>(idSet.size());
+            for (Integer id : idSet) {
+                neighborList.add(this.getAssociatedVertex(id));
+            }
+            return neighborList;
+        } else {
+            
         }
-        return neighborList;
     }
 
     /** Return the number of edges incident to v.  
@@ -159,7 +162,7 @@ public class WGraphP4<VT> implements WGraph<VT> {
             return this.edges.get(v.id()).keySet().size();
         }
         
-        return this.neighbors(v).size();
+        return -1;
     }
 
     /** See if an edge and vertex are incident to each other.
@@ -231,4 +234,7 @@ public class WGraphP4<VT> implements WGraph<VT> {
         return this.edges.get(firstID).remove(secondID);
     }
     
+    private GVertex<VT> getAssociatedVertex(int id) {
+        return this.verts.get(id);
+    }
 }
