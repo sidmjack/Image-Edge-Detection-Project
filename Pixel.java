ee22c946a7f8ea76e3d9adf1bd1cf31c21b997f0
Sidney Jackson <sidneyjackson@sidneys-mbp.mtw.jhu.edu>
2016-04-24 20:38:07 -0400

    Updated getByte function

--- Pixel.java
+++ Pixel.java
@@ -1,25 +1,25 @@
 public class Pixel {
 
-	// Pixel Location
-	private int rowNum;
-	private int colNum;
+    // Pixel Location
+    private int rowNum;
+    private int colNum;
 
-	// Integer Value Containing Image Pixel Informatiom
-	private int pxNum;
+    // Integer Value Containing Image Pixel Informatiom
+    private int pxNum;
 
-	// Pixel Components
-	private int b1; // RGB: Empty, GRAY:
-	private int b2; // RGB: Red, GRAY:
-	private int b3; // RGB: Green, GRAY
-	private int b4; // RGB: Blue, GRAY:
+    // Pixel Components
+    private int b1; // RGB: Empty, GRAY:
+    private int b2; // RGB: Red, GRAY:
+    private int b3; // RGB: Green, GRAY
+    private int b4; // RGB: Blue, GRAY:
 
-	public Pixel(int rN, int cN, int pN) {
-		this.rowNum = rN;
-		this.colNum = cN;
-		this.pxNum = pN;
-	}
+    public Pixel(int rN, int cN, int pN) {
+        this.rowNum = rN;
+        this.colNum = cN;
+        this.pxNum = pN;
+    }
 
-	public int getRow() {
+    public int getRow() {
         return this.rowNum;
     }
 
@@ -28,53 +28,16 @@ public class Pixel {
     }
 
     public int getPixel() {
-    	return this.pxNum;
+        return this.pxNum;
     }
 
-	public int getByte(int byteNum) {
-        
-        int bitMask; // Bit mask used to acquire desired Byte in bitMask.
-        int b; // Byte value to return after pxNum is ANDED with bitMask.
-        
+    public int getByte(int byteNum) {    
+        byteNum = 4-byteNum;
         // Determine the BitMask based off of the input (desired) byteNum.
-        if (byteNum == 1) {
-        	bitMask = 0xF000;
-        } else if (byteNum == 2) {
-        	bitMask = 0x0F00;
-        } else if (byteNum == 3) {
-        	bitMask = 0x00F0;
-        } else if (byteNum == 4) {
-        	bitMask = 0x000F;
+        if (0 <= byteNum && byteNum <= 3) {
+            return (this.pxNum >>> (8 * byteNum)) & 0xFF;
         } else {
-        	bitMask = 0;
+            return 0;
         }
-
-        // AND pxNum with bitMask
-        b = (this.pxNum & bitMask);
-        return b;
-    }
-
-    public int getB1() {
-        int bitMask = 0xF000;
-        int b1 = (this.pxNum & bitMask);
-        return b1;
-    }
-
-    public int getB2() {
-        int bitMask = 0x0F00;
-        int b2 = (this.pxNum & bitMask);
-        return b2;
-    }
-
-    public int getB3() {
-        int bitMask = 0x00F0;
-        int b3 = (this.pxNum & bitMask);
-        return b3;
-    }
-
-    public int getB4() {
-        int bitMask = 0x000F;
-        int b4 = (this.pxNum & bitMask);
-        return b4;
     }
 }
