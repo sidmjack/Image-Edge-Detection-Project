d30eada515b457bfe68709ddadac54e1c1c22dee
Sidney Jackson <sidneyjackson@sidneys-mbp.mtw.jhu.edu>
2016-04-27 18:18:15 -0400

    Worked on diff

--- UnionConditionsCalcs.java
+++ UnionConditionsCalcs.java
@@ -29,8 +29,48 @@ public class UnionConditionsCalcs {
 
 	}
 
-
+	// Has to be inforced that it is the root that is being passed to it.
+	/**
+	 * Confirms whether the "Additional Kruskal's" Condition is met.
+	 * @param  idA Hashmap ID for the desired set A.
+	 * @param  idB Hashmap ID for the desired set B.
+	 * @param  k  : K Value.
+	 * @return     Returns "True" if for operation success.
+	 */
 	public boolean minDiffCond(int idA, int idB, double k) {
+		// Grab the Desired sets.
+		RGBStats setA = this.eqClasses.get(idA);
+		RGBStats setB = this.eqClasses.get(idB);
+		
+		// Initialized/Grab Size of each Set.
+		int sizeA = setA.size;
+		int sizeB = setB.size;
+		
+		// Check to see if the given IDs are actually heads.
+		// How should I check whether the root is being passed to it?
+		
+		// Return whether the condition is satisfied.
+		return (diff(setA, setB) <= min(setA, setB) + k/(sizeA + sizeB));
+	}
+
+	public int diff(RGBStats a, RGBStats b) {
+		// Create new RBGStats Object called 'c'.
+		RGBStats c = new RBGStats();
+		// Determine the max and min for all RGB Stats.
+		c.maxR = Math.max(a.maxR, b.maxR);
+		c.maxG = Math.max(a.maxG, b.maxG);
+		c.maxB = Math.max(a.maxB, b.maxB);
+		c.minR = Math.min(a.maxR, b.maxR);
+		c.minG = Math.min(a.maxG, b.maxG);
+		c.minB = Math.min(a.maxB, b.maxB);
+		// Now determine the differences between min and max.
+		
+		// Return the difference.
+		return c;
+	}
+
+	public RGBStats min(RGBStats a, RGBStats b) {
 
 	}
+
 }
