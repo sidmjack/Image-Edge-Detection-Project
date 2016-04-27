5bfc8cbac46e4dd4daa764fc966817cc24687df7
Lawrence <lawrence@jhu.edu>
2016-04-27 17:34:33 -0400

    UnionConditionsCalcs initial skeleton

---
+++ UnionConditionsCalcs.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Collection;
+
+public class UnionConditionsCalcs {
+	
+	private class RGBStats {
+		
+		int maxR;
+		int maxG;
+		int maxB;
+
+		int minR;
+		int minG;
+		int minB;
+
+		int size;
+
+	}
+
+
+	private HashMap<Integer, RGBStats> eqClasses;
+
+
+	public UnionConditionsCalcs(Collection<Pixel> pixList) {
+
+	}
+
+	public void union(int idA, int idB) {
+
+	}
+
+
+	public boolean minDiffCond(int idA, int idB, double k) {
+
+	}
+}
