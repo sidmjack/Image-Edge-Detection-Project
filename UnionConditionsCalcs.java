bae4406ee8bad1a7f001279689554951ec1fb766
Lawrence <lawrence@jhu.edu>
2016-04-27 17:37:43 -0400

    Collection of Gvertex of Pixel

--- UnionConditionsCalcs.java
+++ UnionConditionsCalcs.java
@@ -21,11 +21,22 @@ public class UnionConditionsCalcs {
 	private HashMap<Integer, RGBStats> eqClasses;
 
 
-	public UnionConditionsCalcs(Collection<Pixel> pixList) {
+	public UnionConditionsCalcs(Collection<GVertex<Pixel>> pixList) {
 
 	}
 
 	public void union(int idA, int idB) {
+		int root1 = this.find(a);     // Find root of node a
+		int root2 = this.find(b);     // Find root of node b
+		if (root1 != root2) {    // Merge with weighted union
+		    if (this.weight[root2] > this.weight[root1]) {
+		        this.parent[root1] = root2;
+		        this.weight[root2] += this.weight[root1];
+		    } else {
+		        this.parent[root2] = root1;
+		        this.weight[root1] += this.weight[root2];
+		    }
+		}
 
 	}
 
