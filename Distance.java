7a53a0b4e6c3b1f8cd1a6905b86308cfb47a036a
Sidney Jackson <sidneyjackson@sidneys-mbp.mtw.jhu.edu>
2016-04-26 20:32:53 -0400

    Distance checkstyled.

--- Distance.java
+++ Distance.java
@@ -1,11 +1,12 @@
 
-/** Distance Interface */
+/** Distance Interface.
+  * @param <VT> Generic. */
 public interface Distance<VT> {
-	/**
-	 * Method that interface is soley comprised of. 
-	 * @param  one First object compared.
-	 * @param  two Second object compared.
-	 * @return     Returns the distance between the two compared objects.
-	 */
-	double distance(VT one, VT two);
+    /**
+     * Method that interface is soley comprised of. 
+     * @param  one First object compared.
+     * @param  two Second object compared.
+     * @return     Returns the distance between the two compared objects.
+     */
+    double distance(VT one, VT two);
 }
